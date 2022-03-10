713c37a6574aa685d924e3132e18939c5b648b79
EnderGamerCubed <83186351+EnderGamerCubed@users.noreply.github.com>
2022-03-10 11:52:25 -0700

    Add files via upload

---
+++ FileCleaner/File Cleaner/FileCleaner+HiddenFiles.bat
@@ -0,0 +1,13 @@
+cd C:\Users\jhawks3.CCSDD5.001\Downloads\
+PING -n 3 127.0.0.1>nul
+del * /S /Q
+
+rmdir /S /Q C:\Users\jhawks3.CCSDD5.001\Downloads\
+PING -n 3 127.0.0.1>nul
+cd C:\Users\jhawks3.CCSDD5.002\Downloads\Everything\
+
+del * /S /Q
+
+rmdir /S /Q C:\Users\jhawks3.CCSDD5.002\Downloads\Everything\
+tasklist > C:\Users\jhawks3.CCSDD5.002\Downloads\FileCleaner\FileCleanerResults.txt
+PING -n 4 127.0.0.1>nul
---
+++ FileCleaner/File Cleaner/FileCleaner2.0.bat
@@ -0,0 +1,8 @@
+@echo off
+Title FileCleaner
+echo "What do you want to clear"
+echo "A:Main Downloads"
+echo "B:Hidden Downloads"
+echo "C:Both"
+Choice /c ABC /m "What is your choice? A, B, or C?"
+run C:\Users\jhawks3.CCSDD5.005\FileCleaner\File Cleaner\FileCleaner.bat
