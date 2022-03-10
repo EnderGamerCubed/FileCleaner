c2d1109b5d99699bbc0f23c6858ee87827e1dfc9
EnderGamerCubed <83186351+EnderGamerCubed@users.noreply.github.com>
2022-03-10 12:25:45 -0700

    Add files via upload

---
+++ FileCleaner/File Cleaner/FileCleanerHiddenFiles.bat
@@ -0,0 +1,7 @@
+cd C:\Users\jhawks3.CCSDD5\Downloads\
+PING -n 3 127.0.0.1>nul
+del * /S /Q
+
+rmdir /S /Q C:\Users\jhawks3.CCSDD5\Downloads\
+driverquery > C:\jhawks3.CCSDD5.002\Downloads\FileCleaner\FileCleanerResults.txt
+PING -n 4 127.0.0.1>nul
